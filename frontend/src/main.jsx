ed61af64917f2278e86d1dc815ec6b329806aebb
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-07 13:54:47 -0300

    Set up React application with WalletProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App.jsx';
+import { WalletProvider } from './wallet/WalletContext.jsx';
+import './styles/index.css';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WalletProvider>
+      <App />
+    </WalletProvider>
+  </React.StrictMode>
+);
 
