212ae0fe818fad20631d9840599f845a326dd432
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-08 19:57:07 -0300

    Refactor translation function and simplify rendering
    Refactored language translation function and removed WalletProvider.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,40 +1,19 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App.jsx';
-import { WalletProvider } from './wallet/WalletContext.jsx';
-import './styles/index.css';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { translations } from './i18n.js' // <-- importa aqui
+import { translations } from './i18n.js'
 
-window.t = (key, lang = 'pt') => translations[lang][key] || key // <-- função global pra usar t('buy')
+const DEFAULT_LANG = 'pt'
+
+window.t = (key, lang = DEFAULT_LANG) => {
+  const dict = translations[lang] || translations[DEFAULT_LANG]
+  return dict[key] || key // se não achar a key, retorna a própria key pra não quebrar
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WalletProvider>
-      <App />
-    </WalletProvider>
-  </React.StrictMode>
-);
 
