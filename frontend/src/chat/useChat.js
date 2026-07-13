bdf22e651103361559ed480da9f7e70a6c5c82f1
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-13 14:03:11 -0300

    Update useChat.js

--- frontend/src/chat/useChat.js
+++ frontend/src/chat/useChat.js
@@ -1,1 +1,9 @@
+import { useContext } from 'react';
+import { ChatContext } from './ChatContext.jsx';
+
+export function useChat() {
+  const ctx = useContext(ChatContext);
+  if (!ctx) throw new Error('useChat must be used within <ChatProvider>.');
+  return ctx;
+}
 
