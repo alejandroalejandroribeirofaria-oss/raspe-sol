df3b003104d9ddcd2954514956e8885c3eab1947
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-14 11:12:54 -0300

    Wrap AdminDashboard in I18nProvider

--- frontend/src/admin/AdminApp.jsx
+++ frontend/src/admin/AdminApp.jsx
@@ -1,1 +1,12 @@
+import { I18nProvider } from '../i18n/I18nContext';
+import AdminDashboard from './AdminDashboard.jsx';
+import '../styles/index.css';
+
+export default function AdminApp() {
+  return (
+    <I18nProvider>
+      <AdminDashboard />
+    </I18nProvider>
+  );
+}
 
