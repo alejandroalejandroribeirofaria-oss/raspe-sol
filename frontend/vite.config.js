9a8a51822f3aabf22e8415ae43383df217c37822
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-14 21:49:11 -0300

    Set base path for Vite configuration
    Added base configuration for Vite.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
+  // ← Adicione isso
+  base: '/',
+
   server: {
     port: 5173,
   },
 
   build: {
-    // Aumenta o limite do aviso de tamanho dos chunks (em KB)
     chunkSizeWarningLimit: 1000,
 
-    // Separa bibliotecas grandes em arquivos independentes
     rollupOptions: {
       output: {
         manualChunks: {
