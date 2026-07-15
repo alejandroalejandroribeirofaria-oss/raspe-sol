e84a758f05fc217388b1954da2f41adf309779b6
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-15 13:47:03 -0300

    Remove WalletProvider from main.jsx
    Removed WalletProvider from the main rendering.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { WalletProvider } from './wallet/WalletProvider.jsx';
 import { I18nProvider } from './i18n/I18nContext.jsx';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nProvider>
-      <WalletProvider>
-        <App />
-      </WalletProvider>
+      <App />
     </I18nProvider>
   </React.StrictMode>
 );
