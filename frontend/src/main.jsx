91cf19469a4f28c085bd225b4958c7c951ccbbb1
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-19 13:01:16 -0300

    Change I18nProvider import from .jsx to .js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './styles/index.css' 
 import '@solana/wallet-adapter-react-ui/styles.css' // <- PRECISA DISSO PRA MODAL DA WALLET FUNCIONAR
 
-import { I18nProvider } from './i18n/I18nProvider.jsx'
+import { I18nProvider } from './i18n/I18nProvider.js'
 import { WalletProvider } from './wallet/WalletProvider.jsx'
 import { ChatProvider } from './chat/ChatProvider.jsx'
 
