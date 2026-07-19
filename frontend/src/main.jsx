8abbfdd37a7a573bd2e7889596c90cade5946dc0
alejandroalejandroribeirofaria-oss <alejandroalejandroribeirofaria@gmail.com>
2026-07-19 14:32:42 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import App from './App.jsx'
-import './styles/index.css' 
-import '@solana/wallet-adapter-react-ui/styles.css' // <- PRECISA DISSO PRA MODAL DA WALLET FUNCIONAR
+import './styles/index.css'
+import '@solana/wallet-adapter-react-ui/styles.css' // estilos da wallet
 
 import { I18nProvider } from './i18n/I18nProvider.jsx'
 import { WalletProvider } from './wallet/WalletProvider.jsx'
@@ -11,11 +12,11 @@ import { ChatProvider } from './chat/ChatProvider.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nProvider>
-      <WalletProvider> {/* 1º: Wallet tem que vir primeiro */}
-        <ChatProvider> {/* 2º: Chat usa o Wallet */}
+      <WalletProvider>
+        <ChatProvider>
           <App />
         </ChatProvider>
       </WalletProvider>
     </I18nProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
